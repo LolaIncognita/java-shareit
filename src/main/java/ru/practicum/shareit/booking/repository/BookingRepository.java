8e5f420d9d5bb27dc56f4a7550e269ecece29d50
Lolita <LolitaKardan@yandex.ru>
2023-09-19 06:03:19 +0300

    style: deleate unused imports

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.booking.dto.BookingClosest;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;
-import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
